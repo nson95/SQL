4cc982acaa2a04ba9a56744a22b04f91900e6b25
nson95 <nicksonnenberg95@gmail.com>
2018-08-30 14:27:57 -0400

    another practice sql for deleting and updating

--- day4sqlscript.sql
+++ day4sqlscript.sql
@@ -4,3 +4,21 @@ SELECT * from accounts
   UPDATE accounts
      SET balance = balance + 100
    WHERE balance <=4000;
+   
+SELECT * FROM accounts 
+ WHERE accountid = 101;
+ 
+ DELETE 
+   FROM accounts 
+  WHERE accountid = 101;
+ 
+ SELECT * from accounts
+  WHERE accountid in (50,51,100,101);
+  
+SELECT * FROM accounts
+ WHERE account_holder like '% J%';
+ 
+
+SELECT DISTINCT balance
+		   FROM accounts;
+ 
