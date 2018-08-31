784ec5fc9d649a637cc634fb532d3dd828fa08f9
nson95 <nicksonnenberg95@gmail.com>
2018-08-31 09:06:48 -0400

    fixed it!

--- homework-8.30.18.sql
+++ homework-8.30.18.sql
@@ -1,10 +1,11 @@
 SELECT *
   FROM invoice i, lineitem li, product p
- where i.id = li.id and p.id = li.id;
+ where i.id = li.id and p.id =
+ li.id;
  
  
- SELECT * 
+ SELECT i.id, i.OrderNumber, i.OrderDate, i.total, li.id, li.InvoiceID, li.ProductID,
+ li.quantity, p.id, p.code as Book, p.Description, p.listprice
    FROM invoice i
-   JOIN lineitem li ON i.id = li.id
-   JOIN product p ON li.id  = 
-   p.id;
+   JOIN lineitem li ON li.invoiceid = i.id
+   JOIN product p ON li.productid  = p.id;
