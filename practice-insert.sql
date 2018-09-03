38827e5a0ee8e0986e538575594a1c9528bd663b
nson95 <nicksonnenberg95@gmail.com>
2018-09-02 20:47:06 -0400

    more info for prs user database

--- practice-insert.sql
+++ practice-insert.sql
@@ -1,4 +1,5 @@
 SELECT * FROM user;
 
 INSERT into user(UserName, Password, FirstName, LastName, PhoneNumber, Email, IsReviewer, IsAdmin, IsActive)
-		 VALUES ('nsonnenberg', '123abc', 'Nick', 'Sonnenberg', '513-373-1999','nicksonnenberg95@gmail.com', 1, 1, 1);
+		 VALUES ('nsonnenberg', 'password', 'Nick', 'Sonnenberg', '513-373-1999','nicksonnenberg95@gmail.com', 1, 1, 1),
+				('larrydavid', '1234', 'Larry', 'David', '555-554-2183', 'ldav@hotmail.com', 0, 0, 1);
