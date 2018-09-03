6c6e0118ad49ff807d8a55832b7a99293f4a6457
nson95 <nicksonnenberg95@gmail.com>
2018-09-03 11:27:23 -0400

    updates for PRS db

--- practice insert.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM user;
-
-INSERT into user(UserName, Password, FirstName, LastName, PhoneNumber, Email, IsReviewer, IsAdmin, IsActive)
-		 VALUES ('nsonnenberg', '123abc', 'Nick', 'Sonnenberg', '513-373-1999','nicksonnenberg95@gmail.com', 1, 1, 1);
--- practice-insert.sql
+++ practice-insert.sql
@@ -2,4 +2,5 @@ SELECT * FROM user;
 
 INSERT into user(UserName, Password, FirstName, LastName, PhoneNumber, Email, IsReviewer, IsAdmin, IsActive)
 		 VALUES ('nsonnenberg', 'password', 'Nick', 'Sonnenberg', '513-373-1999','nicksonnenberg95@gmail.com', 1, 1, 1),
-				('larrydavid', '1234', 'Larry', 'David', '555-554-2183', 'ldav@hotmail.com', 0, 0, 1);
+				('larrydavid', '1234', 'Larry', 'David', '555-554-2183', 'ldav@hotmail.com', 0, 0, 1),
+                ('dchap', 'Africa', 'Dave', 'Chappelle', '555-554-7934', 'dchappelle@hotmail.com', 0, 0, 1);
