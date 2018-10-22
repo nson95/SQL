2882d01c9bb46d1398dd49ed25341d1e6c375a2b
nson95 <nicksonnenberg95@gmail.com>
2018-10-22 15:36:01 -0400

    script for creating bmdb schema

---
+++ insert-bmd.sql
@@ -0,0 +1,20 @@
+insert into Movie VALUES
+ 	(3, 'Annie Hall', 1977),
+    (4, 'Bananas', 1971),
+    (5, 'Big', 1988),
+    (6, 'Saving Private Ryan', 1998),
+    (7, 'Ocean\'s Eleven', 2001),
+    (8, 'Up in the Air', 2009),
+    (9, 'Hancock', 2008),
+    (10, 'Shark Tale', 2004),
+    (11, 'Taxi Driver', 1976);
+    insert into Movie VALUES
+    (12, 'Casino', 1995),
+    (13, 'One Flew Over the Cuckoo\'s Nest', 1975),
+    (14, 'As Good as It Gets', 1997),
+    (15, 'Invincible', 2006),
+    (16, 'Catch Me if You Can', 2002);
+     Select *
+       FROM movie;
+       
+       
